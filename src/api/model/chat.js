171652743cebaa91d5d88f170b6a640c94f4141a
lyh <lyh_night@163.com>
2026-01-20 21:35:21 +0800

    feat(backup): 备份

--- src/api/model/chat.js
+++ src/api/model/chat.js
@@ -1,6 +1,6 @@
 import instance from '../http.js'
 
-import chatList from './mock/chatList.json'
+import chatList from '../mock/chatList.json'
 
 export default {
   getChatList(params) {
@@ -8,8 +8,11 @@ export default {
     // return instance.post('/api/chat/list', params)
   },
   getChatDetail(params) {
-    const children = chatList.data.find((item) => item.id == params.id).children
-    return Promise.resolve({ code: 0, data: children })
+    const chatItem = chatList.data.find((item) => item.id == params.id)
+    if (!chatItem) {
+      return Promise.resolve({ code: 404, msg: '对话不存在', data: [] })
+    }
+    return Promise.resolve({ code: 0, data: chatItem.children })
     // return instance.post('/api/chat/detail', params)
   }
 }
